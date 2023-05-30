ae60dfbe54f24d3814930e74c9c86956ed5b0580
Reynaldi1912 <nexrey19@gmail.com>
2023-05-30 22:43:51 +0700

    update api store exception

--- app/Http/Controllers/ExceptionMobileController.php
+++ app/Http/Controllers/ExceptionMobileController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Exception;
+use App\Models\Jadwal;
+use App\Models\User;
+use DB;
 class ExceptionMobileController extends Controller
 {
     /**
@@ -35,7 +38,31 @@ class ExceptionMobileController extends Controller
      */
     public function store(Request $request)
     {
-        
+        $getJadwal = Jadwal::all()->where('tanggal_mulai',$request->tanggal)->where('id_desa',$request->id_desa)->first();
+
+        if($getJadwal != null){
+            $cekException = Exception::all()->where('id_jadwal',$getJadwal->id)->where('id_user',$request->id);
+            if($cekException->isNotEmpty()){
+                return response()->json([
+                    'message' => 'Exception Tanggal '.$request->tanggal.' Anda Sudah Diajukan',
+                ], 200);
+            }else{
+                Exception::create([
+                    'id_jadwal' => $getJadwal->id,
+                    'id_user' => $request->id,
+                    'exception_status' => $request->status,
+                    'alasan' => $request->alasan,
+                    'status_appr' => 0,
+                ]);
+                return response()->json([
+                    'message' => 'Data berhasil disimpan',
+                ], 200);
+            }
+        }else{
+            return response()->json([
+                'message' => 'Anda Tidak Ada Jadwal Pada Tanggal '.$request->tanggal,
+            ], 200);
+        }
     }
 
     /**

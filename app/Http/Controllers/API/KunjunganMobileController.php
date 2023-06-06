659bfe2382e2655f4f50fa139584430309becf08
Reynaldi1912 <nexrey19@gmail.com>
2023-06-06 23:31:13 +0700

    updated

--- app/Http/Controllers/API/KunjunganMobileController.php
+++ app/Http/Controllers/API/KunjunganMobileController.php
@@ -37,61 +37,37 @@ class KunjunganMobileController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-
     public function store(Request $request)
     {
         $image = $request->file('image');
 
         // Menyimpan gambar ke direktori yang diinginkan
         $imageName = $image->getClientOriginalName();
+        $image->move(public_path('images'), $imageName);
 
-        // Menggunakan Intervention Image untuk mengkonversi ukuran gambar
-        $resizedImage = Image::make($image->getRealPath())->resize(800, null, function ($constraint) {
-            $constraint->aspectRatio();
-            $constraint->upsize();
-        });
+        Pasien::create([
+            'nik' => $request->nik ,
+            'nama' =>$request->nama ,
+            'jml_anggota_keluarga' => $request->jml_anggota_keluarga ,
+            'tgl_lahir' => $request->tanggal_lahir ,
+            'umur' => $request->umur ,
+            'alamat' => $request->alamat ,
+            'no_hp' => $request->no_hp ,
+            'bpjs' => $request->bpjs ,
+            'created_by' =>$request->created_by
+        ]);
 
-        // Menyimpan gambar yang sudah diresize
-        $resizedImage->save(public_path('images') . '/' . $imageName, 80);
-
-        // Cek ukuran gambar setelah diresize
-        $imageSize = $resizedImage->filesize();
-
-        if ($imageSize <= 2 * 1024 * 1024) {
-            // Ukuran gambar sesuai dengan batasan yang diinginkan (di bawah 2 MB)
-            // Lanjutkan dengan menyimpan data ke database atau lakukan tindakan lain yang diperlukan
-            Pasien::create([
-                'nik' => $request->nik,
-                'nama' => $request->nama,
-                'jml_anggota_keluarga' => $request->jml_anggota_keluarga,
-                'tgl_lahir' => $request->tanggal_lahir,
-                'umur' => $request->umur,
-                'alamat' => $request->alamat,
-                'no_hp' => $request->no_hp,
-                'bpjs' => $request->bpjs,
-                'created_by' => $request->created_by
-            ]);
-
-            Hasil_Kunjungan::create([
-                'nik' => $request->nik,
-                'berat_badan' => (float) $request->berat_badan,
-                'tinggi_badan' => $request->tinggi_badan,
-                'tekanan_darah' => $request->tekanan_darah,
-                'penyuluhan' => $request->penyuluhan,
-                'diagnosa' => $request->diagnosa,
-                'dokumentasi' => $imageName,
-                'created_by' => $request->created_by
-            ]);
-        } else {
-            // Ukuran gambar melebihi batasan yang diinginkan (di atas 2 MB)
-            // Lakukan tindakan yang sesuai seperti memberikan notifikasi kepada pengguna atau menolak penyimpanan gambar
-            // ...
-
-            // Hapus gambar yang telah diupload karena ukurannya terlalu besar
-            unlink(public_path('images') . '/' . $imageName);
-        }
+        Hasil_Kunjungan::create([
+            'nik' => $request->nik ,
+            'berat_badan' => (float) $request->berat_badan,
+            'tinggi_badan' => $request->tinggi_badan,
+            'tekanan_darah' => $request->tekanan_darah,
+            'penyuluhan' => $request->penyuluhan,
+            'diagnosa' => $request->diagnosa,
+            'dokumentasi' => $imageName,
+            'created_by' => $request->created_by
+        ]);
     }
-
 
     /**
      * Display the specified resource.

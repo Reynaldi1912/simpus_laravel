3ab69adaae25d0b6e0304a0756e769e637010a36
Reynaldi1912 <nexrey19@gmail.com>
2023-07-20 08:59:02 +0700

    update

--- resources/views/dana_kunjungan/index.blade.php
+++ resources/views/dana_kunjungan/index.blade.php
@@ -5,52 +5,48 @@
     <div class="main-container">
         <div class="content">
             <div class="block">
-            <div class="p-3 block-header-default">
-                <div class="row">
-                    <div class="col-6">
-                        <h3 class="block-title">Dana Kunjungan</h3>
+                <div class="p-3 block-header-default">
+                    <div class="row">
+                        <div class="col-6">
+                            <h3 class="block-title">Dana Kunjungan</h3>
+                        </div>
+                        <div class="col-6">
+                            <input type="number" class="form-control" id="dana_kunjungan" placeholder="Dana Kunjungan">
+                        </div>
                     </div>
-                    <div class="col-6">
-                        <input type="number" class="form-control" id="dana_kunjungan" placeholder="Dana Kunjungan">
-                    </div>
-                    <div class="block-content block-content-full">
-                <!-- DataTables functionality is initialized with .js-dataTable-full class in js/pages/be_tables_datatables.min.js which was auto compiled from _es6/pages/be_tables_datatables.js -->
-                <table class="table table-bordered table-striped table-vcenter js-dataTable-full">
-                    <thead>
-                        <tr>
-                            <th class="text-center">No</th>
-                            <th class="text-center">Nama Desa</th>
-                            <th class="text-center">Bulan</th>
-                            <th class="text-center">Kunjungan</th>
-                            <th class="text-center">Tidak Kunjungan</th>
-                            <th class="text-center">Total Kunjungan</th>
-                            <th class="text-center">Total Dana Kunjungan</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @php
-                        $i = 1;
-                            @endphp
-                        @foreach($data as $key)
+                </div>
+                <div class="block-content block-content-full">
+                    <table class="table table-bordered table-striped table-vcenter" id="datatable">
+                        <thead>
                             <tr>
-                                <td>{{$i++}}</td>
-                                <td>{{$key->nama_desa}}</td>
-                                <td>{{$key->nama_bulan}}</td>
-                                <td class="text-center kunjungan" data-value="{{ (int)$key->kunjungan }}">{{ $key->kunjungan }} Kunjungan</td>
-                                <td>{{$key->total_kunjungan - $key->kunjungan}} Kunjungan</td>
-                                <td>{{$key->total_kunjungan}} Kunjungan</td>
-                                <td class="total-dana-kunjungan">Rp 0,00</td>
+                                <th class="text-center">No</th>
+                                <th class="text-center">Nama Desa</th>
+                                <th class="text-center">Bulan</th>
+                                <th class="text-center">Kunjungan</th>
+                                <th class="text-center">Tidak Kunjungan</th>
+                                <th class="text-center">Total Kunjungan</th>
+                                <th class="text-center">Total Dana Kunjungan</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+                        </thead>
+                        <tbody>
+                            @php
+                            $i = 1;
+                            @endphp
+                            @foreach($data as $key)
+                                <tr>
+                                    <td>{{$i++}}</td>
+                                    <td>{{$key->nama_desa}}</td>
+                                    <td>{{$key->nama_bulan}}</td>
+                                    <td class="text-center kunjungan" data-value="{{ (int)$key->kunjungan }}">{{ $key->kunjungan }} Kunjungan</td>
+                                    <td>{{$key->total_kunjungan - $key->kunjungan}} Kunjungan</td>
+                                    <td>{{$key->total_kunjungan}} Kunjungan</td>
+                                    <td class="total-dana-kunjungan">Rp 0,00</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
-            <div class="block-content block-content-full">
-                
-            </div>
-        </div>
         </div>
     </div>
 </div>
@@ -58,38 +54,30 @@
 <script>
     $(document).ready(function () {
         var table = $('#datatable').DataTable({
-            pageLength: 15, // Ubah nilai ini sesuai kebutuhan Anda (lebih dari 13)
-            lengthMenu: [[15, 25, 30, -1], [15, 25, 30, 'Semua']]
+            pageLength: 15,
+            lengthMenu: [[15, 25, 30, -1], [15, 25, 30, 'Todos']]
         });
 
         document.getElementById('dana_kunjungan').addEventListener('input', function () {
-            var danaKunjungan = parseFloat(this.value.replace(/[^\d.-]/g, ""));
+            var danaKunjungan = parseFloat(this.value);
             var totalDanaKunjunganCells = document.getElementsByClassName('total-dana-kunjungan');
 
             Array.from(totalDanaKunjunganCells).forEach(function (cell) {
-                var kunjunganText = cell.parentElement.querySelector('.kunjungan').getAttribute('data-value');
+                var kunjunganText = cell.parentNode.querySelector('.kunjungan').getAttribute('data-value');
                 var kunjungan = parseInt(kunjunganText);
 
-                console.log(kunjungan);
                 if (!isNaN(kunjungan)) {
                     var totalDanaKunjungan = kunjungan * danaKunjungan;
-                    var formattedDanaKunjungan = formatRupiah(totalDanaKunjungan);
+                    var formattedDanaKunjungan = totalDanaKunjungan.toLocaleString('id-ID', {
+                        style: 'currency',
+                        currency: 'IDR'
+                    });
                     cell.textContent = formattedDanaKunjungan;
                 } else {
                     cell.textContent = "-";
                 }
             });
         });
-
-        // Function untuk mengubah angka menjadi format Rupiah
-        function formatRupiah(angka) {
-            var formatter = new Intl.NumberFormat('id-ID', {
-                style: 'currency',
-                currency: 'IDR',
-                minimumFractionDigits: 2
-            });
-            return formatter.format(angka);
-        }
     });
 </script>
 
